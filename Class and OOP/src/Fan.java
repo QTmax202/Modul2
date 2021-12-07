2062e416bfb62a9eec5aee2c6050055ad071b850
QTmax202 <89342545+QTmax202@users.noreply.github.com>
2021-12-07 14:36:43 +0700

    Class and OOP(08/12/2021)

--- Class and OOP/src/Fan.java
+++ Class and OOP/src/Fan.java
@@ -1,17 +1,34 @@
 public class Fan {
-    int speed, radius;
+    int speed;
+    double radius;
     boolean turnOnOff;
     String color;
 
-    public Fan( int speed, int radius, String color) {
-        this.speed = speed;
+    public Fan() {
+        this.speed = 1;
         this.turnOnOff = false;
-        this.radius = radius;
+        this.radius = 5;
+        this.color = "blue";
+    }
+
+    public int setSpeed(int speed){
+        this.speed = speed;
+        return this.speed;
+    }
+
+    public String setColor(String color){
         this.color = color;
+        return this.color;
+    }
+
+    public double setRadius(double radius){
+        this.radius = radius;
+        return this.radius;
     }
 
     public boolean turnOnOff() {
-        return !this.turnOnOff;
+        this.turnOnOff =!this.turnOnOff;
+        return this.turnOnOff;
     }
 
     public String Speed() {
@@ -31,7 +48,7 @@ public class Fan {
         if (this.turnOnOff){
             return Speed() + ", Radius: " + this.radius + ", Color: " + this.color + ", Quạt đang bật" ;
         } else {
-            return "Radius: " + this.radius + ", Color: " + this.color + "Quạt đang tắt";
+            return "Radius: " + this.radius + ", Color: " + this.color + ", Quạt đang tắt";
         }
     }
 }
--- Class and OOP/src/mainFan.java
+++ Class and OOP/src/mainFan.java
@@ -1,8 +1,11 @@
 public class mainFan {
     public static void main(String[] args) {
-        Fan fan1 = new Fan(2, 10, "Yellow");
-        Fan fan2 = new Fan(3, 5, "blue");
+        Fan fan1 = new Fan();
+        Fan fan2 = new Fan();
         fan1.turnOnOff();
+        fan1.setSpeed(3);
+        fan1.setColor("yellow");
+        fan1.setRadius(10);
         System.out.println(fan1.toString());
         System.out.println(fan2.toString());
     }
