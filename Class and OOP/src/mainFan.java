64bbac774e73250d7293ef33a12b418474a45aa4
QTmax202 <89342545+QTmax202@users.noreply.github.com>
2021-12-07 14:18:44 +0700

    Class and OOP(08/12/2021)

--- Class and OOP/src/mainFan.java
+++ Class and OOP/src/mainFan.java
@@ -2,6 +2,7 @@ public class mainFan {
     public static void main(String[] args) {
         Fan fan1 = new Fan(2, 10, "Yellow");
         Fan fan2 = new Fan(3, 5, "blue");
+        fan1.turnOnOff();
         System.out.println(fan1.toString());
         System.out.println(fan2.toString());
     }
