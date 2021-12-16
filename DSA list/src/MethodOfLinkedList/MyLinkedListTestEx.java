e670049b8912d953ea21f0ca4887bf4e43047765
QTmax202 <89342545+QTmax202@users.noreply.github.com>
2021-12-16 13:41:02 +0700

    List(16-12-2021)

--- DSA list/src/MethodOfLinkedList/MyLinkedListTestEx.java
+++ DSA list/src/MethodOfLinkedList/MyLinkedListTestEx.java
@@ -1,4 +1,19 @@
 package MethodOfLinkedList;
 
 public class MyLinkedListTestEx {
+    public static void main(String[] args) {
+        MyLinkedListEx list = new MyLinkedListEx(1);
+        list.add(1, 2);
+        list.add(2,"Melinda");
+        list.add(3,4);
+        list.add(4,5);
+        list.addFirst(0);
+        list.addLast(73);
+        list.printList();
+        System.out.println("-------");
+        list.get(6);
+        System.out.println(list.indexOf("Melinda"));
+        System.out.println(list.contains("Melinda"));
+        list.size();
+    }
 }
